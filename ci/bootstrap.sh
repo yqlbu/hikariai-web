c3e20644966777a99fa89ca8bca6e79ee78c1759
yqlbu <yqlbu@bu.edu>
2021-10-16 00:51:49 +0800

    updated all

--- ci/bootstrap.sh
+++ ci/bootstrap.sh
@@ -3,7 +3,7 @@
 echo "Modifying baseURL ..."
 if [[ $ENV == "prod" || $ENV == "staging" ]]; then
   echo "New baseURL: https://$DOMAIN_NAME";
-  sed -i 's/baseURL=.*/baseURL="https\:\/\/'$DOMAIN_NAME'"/g' config.toml;
+  #sed -i 's/baseURL=.*/baseURL="https\:\/\/'$DOMAIN_NAME'"/g' config.toml;
 else
   echo "New baseURL: http://$SERVER_IP";
   sed -i 's/baseURL=.*/baseURL="http\:\/\/'$SERVER_IP'"/g' config.toml;
